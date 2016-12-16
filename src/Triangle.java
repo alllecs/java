c6a93b696ec9ef60ddfd0cf45f35425930dee40a
Alexander Smirnov <alllecs@yandex.ru>
2016-12-16 16:54:22 +0300

    В переменных a и b лежат положительные длины катетов прямоугольного треугольника. Вычислить и вывести на экран площадь треугольника и его периметр.

---
+++ src/Triangle.java
@@ -0,0 +1,9 @@
+public class Triangle {
+	public static void main(String[] args) {
+		int a = 5;
+		int b = 8;
+		int i = a * b / 2;
+		double j = a + b + (Math.sqrt((a * a) + (b * b)));
+		System.out.println("S = " + i + ", P = " + j);
+	}
+}
