a2c0d070224fff07750760a3e813c412af75f07f
Alexander Smirnov <alllecs@yandex.ru>
2016-12-19 12:04:54 +0300

    Создать программу, проверяющую и сообщающую на экран, является ли целое число записанное в переменную n, чётным либо нечётным.

---
+++ src/Chet.java
@@ -0,0 +1,10 @@
+public class Chet {
+	public static void main(String[] args) {
+		int i = 23;
+		if (i % 2 == 1) {
+			System.out.println("Нечетное");
+		} else {
+			System.out.println("Четное");
+		}
+	}
+}
