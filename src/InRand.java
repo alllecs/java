8dc476ca589be7722b5840ef82491c7e32af99cb
Alexander Smirnov <alllecs@yandex.ru>
2017-01-10 16:14:13 +0300

    Программа загадывает случайное целое число из отрезка [1;10] и просит пользователя его угадать, вводя варианты с клавиатуры, пока пользователь не угадает число, программа будет ему подсказывать, сообщая больше или меньше число загаданное, чем то, что ввёл пользователь

---
+++ src/InRand.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+public class InRand {
+   public static void main(String[] args) {
+	// prog — число созданное программой
+	// user — число введённое пользователем
+	int prog, user;
+	int i = 0;
+	// Генерируем случайное целое число от 1 до 10
+	prog = (int)(Math.random() * 10) + 1;
+	System.out.println("Я загадала число от 1 до 10, отгадайте его.");
+	System.out.print("Вводите ваше число: ");
+	Scanner input = new Scanner(System.in);  
+	// Проверяем, есть ли в потоке ввода целое число
+	if( input.hasNextInt() ) {
+		do {
+			// Читаем с потока ввода целое число
+			user = input.nextInt();
+			if(user == prog) {
+				System.out.println("Вы угадали!");
+			} else {
+				// Проверяем, входит ли число в отрезок [1;10]
+				if (user > 0 && user <= 10) {
+					System.out.print("Вы не угадали! ");
+					// Если число загаданное программой меньше...
+					if( prog < user ) {
+						System.out.println("Моё число меньше.");
+					} else {
+						System.out.println("Моё число больше.");
+					}
+				} else {
+					System.out.println("Ваше число вообще не из нужного отрезка!");
+				}
+			}
+		} while( user != prog );
+	} else {
+		System.out.println("Ошибка. Вы не ввели целое число!");
+	}
+	System.out.println("До свиданья!");
+   }
+}
